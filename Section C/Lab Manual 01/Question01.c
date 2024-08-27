a0b4417ff5f18ffea6a3d8188e1b3ededcafcd88
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-08-27 22:50:57 +0500

    Added Header File

--- Section C/Lab Manual 01/Question01.c
+++ Section C/Lab Manual 01/Question01.c
@@ -1,19 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <sys/mman.h>
-#include <sys/shm.h>
-#include <sys/ipc.h>
-#include <signal.h>
-#include <stdbool.h>
-#include <string.h>
-#include <time.h>
+#include "Header.h"
 
 int main(int argc, char *argv[])
 {
