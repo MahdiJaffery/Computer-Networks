85650ac2fec7e97bb1f555e8d4945b11d9e7dbab
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-10-15 17:40:35 +0500

    UDP Client

---
+++ Section H/Lab Manual 05/Client.py
@@ -0,0 +1,22 @@
+import socket
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    server_address = ('127.0.0.1', 2000)
+
+    try:
+        client_message = input("Enter Message: ")
+
+        sock.sendto(client_message.encode(), server_address)
+
+        server_message, _ = sock.recvfrom(2000)
+        print(f"Server Message: {server_message.decode()}")
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        sock.close()
+
+main()
